205a7d2064288a9a252d9cb18ba1b91aa764e250
Tanveer Ahmad <tanveerisonline@gmail.com>
2023-01-18 20:30:09 +0530

    Add routing to math_magician project

---
+++ src/components/Quote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Quote = () => (
+  <div style={{
+    height: 'calc( 70vh - 100px )',
+    fontSize: '30px',
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: '0 60px',
+  }}
+  >
+    Mathematics is not about numbers, equations, computations,
+    or algorithms: it is about understanding. - William Paul Thurston
+  </div>
+);
+
+export { Quote as default };
